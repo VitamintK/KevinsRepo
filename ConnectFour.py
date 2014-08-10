39f874e3ab76ec693eeed50b4944b97a7b0fe933
KEvin Wang <kvn.a.wng@gmail.com>
2014-08-10 02:36:28 -0700

    beginning Connect Four program

--- ConnectFour.py
+++ ConnectFour.py
@@ -1,6 +1,20 @@
 class Board:
+    def __init__(self,x,y,winlength):
+        self.board = [[' ' for i in xrange(y)] for j in xrange(x)]
+        self.winlength = winlength
+    def visualize(self):
+        for column in [list(i) for i in reversed(zip(*self.board))]:
+            print ".".join(column)
+    def check_game_over():
+        pass
+    def add_piece(self,column,piece):
+        for index, space in enumerate(self.board[column]):
+            if space == ' ':
+                self.board[column][index] = piece
+                return True
+        else:
+            return False
+
+class Game:
     def __init__(self):
-        self.board = [['0' for x in xrange(7)] for y in xrange(6)]
-    def graph(self):
-        for row in self.board:
-            print " ".join(row)
+        self.game_board = Board(7,6,4)
