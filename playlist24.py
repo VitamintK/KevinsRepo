563140cea60492c65790f0b6756aba0720a60e63
KEvin Wang <kvn.a.wng@gmail.com>
2014-09-20 15:44:42 -0700

    moved ConnectFour to its own repo, started playlist project

---
+++ playlist24.py
@@ -0,0 +1,9 @@
+"""24 hour playlist"""
+
+class Playlist:
+    def __init__(self, audio_files, name = "playlist24"):
+        self.audio_files = audio_files
+    def save_m3u(self):
+        pass
+
+#m3u: absolute, relative, 
